db33270260f238784da63b91d9df14ddfcee630a
jakewang <jakewang@chileaf.com>
2021-03-30 10:41:44 +0800

    add offline & cadence distance

--- src/main/java/com/chileaf/cl900/ws/CL900Runner.java
+++ src/main/java/com/chileaf/cl900/ws/CL900Runner.java
@@ -11,18 +11,29 @@ import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Component;
 
 import java.io.IOException;
-import java.util.Date;
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
+import java.util.concurrent.ConcurrentHashMap;
 
 @Slf4j
 @Order(1)
 @Component
 public class CL900Runner implements ApplicationRunner {
 
+    private static final int mWheel = 2340;
+    private static final long mOffline = 10000;
+    private static final ConcurrentHashMap<String, Long> mCadence = new ConcurrentHashMap<>();
+    private static final ConcurrentHashMap<String, Long> mCacheTime = new ConcurrentHashMap<>();
+
     @Override
     public void run(ApplicationArguments args) {
-        log.info("------------->" + "项目启动，now =" + new Date());
+        log.info("开始解析设备数据，时间:" + new Date());
+        Timer timer = new Timer();
+        timer.scheduleAtFixedRate(new TimerTask() {
+            @Override
+            public void run() {
+                checkOffline();
+            }
+        }, 0, 5000);
         ChileafApi api = ChileafApiUtil.getInstance();
         api.isDubug(false);
         api.setPerimeter(2340);
@@ -30,35 +41,29 @@ public class CL900Runner implements ApplicationRunner {
             @Override
             public void onComplete(Object result) {
                 parseJson(api, result);
-//                String cmd = api.getCmd();
-//                System.out.println("hubid:" + api.getHubid() + "\tcommand：" + cmd);
-//                try {
-//                    WebSocketServer.sendMessage(api.getHubid() + api.getDeviceType(), null);
-//                } catch (IOException e) {
-//                    e.printStackTrace();
-//                }
-//                if ("01".equals(cmd)) {
-//                    String Devicetyp = api.getDeviceType();
-//                    if ("Heart Rate Device".equals(Devicetyp)) {
-//                        //只有当日志中的数据中的某个是00，80才会出卡路里、步数，只有当是07得时候才有电池
-//                        System.out.println("Battery level：" + api.getBattery());
-//                        System.out.println("Device ID：" + api.getDeviceID() + "\theart rate：" + api.getHeartrate() + "\tSteps:" + api.getStep() + "\tcalorie:" + api.getCalorie() + "(Kcal)"); //If there are calories, there are no step. If there are step, there are no calories
-//                    } else if ("Rope skipping".equals(Devicetyp)) {
-//                        System.out.println("Device ID：" + api.getDeviceID() + "\tCounter: " + api.getCounter() + "\tActivity time: " + api.getTime() + "s");
-//                    } else if ("cadence".equals(Devicetyp)) {
-//                        System.out.println("Device ID：" + api.getDeviceID() + "\tcadence: " + api.getCadence() + " RPM");
-//                    } else if ("speed".equals(Devicetyp)) {
-//                        System.out.println("Device ID：" + api.getDeviceID() + "\tspeed: " + api.getSpeed() + " km/h");
-//                    }
-//                    System.out.println("\tRssi" + api.getRssi());
-//                } else if ("02".equals(cmd)) {
-//                    System.out.println("Bluetooth name:" + api.getBleName());
-//                } else if ("04".equals(cmd)) {
-//                    //getDataSourceSelection:00:ant  01ble  02:ant+Bluetooth  ff:All shut down
-//                    System.out.println("Transmission frequency：" + api.getFrequency() + "\thub data sources:" + api.getDataSourceSelection() + "\tRemark length:" + api.getRemarks() + "\t" + "Limit Bluetooth name length：" + api.getBlethNamelen() + "\trestricted scan Bluetooth UUID length：" + api.getBleUUIDlen() + "\tRestricted scan of Bluetooth rssis：" + api.getRssi() + "\tIntranet / Extranet logo：" + api.getSignals() + "\tIP length of server / TV box：" + api.getIPLength() + "\tPort length of server box：" + api.getPortlen());
-//                }
             }
         });
+    }
+
+    private synchronized void checkOffline() {
+        log.info("缓存设备列表:" + mCacheTime.toString());
+        for (Map.Entry<String, Long> next : mCacheTime.entrySet()) {
+            long current = System.currentTimeMillis();
+            long delta = current - next.getValue();
+            if (delta >= mOffline) {
+                Map<String, Object> cache = new HashMap<>();
+                cache.put("deviceId", next.getKey());
+                cache.put("deviceType", "-1");
+                try {
+                    WebSocketServer.sendMessage(JSONObject.toJSONString(cache), null);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+                log.info("移除离线设备:" + next.getKey());
+                mCacheTime.remove(next.getKey());
+                mCadence.remove(next.getKey());
+            }
+        }
     }
 
     /**
@@ -73,13 +78,18 @@ public class CL900Runner implements ApplicationRunner {
             String cmd = api.getCmd();
             String deviceId = api.getDeviceID();
             if (!deviceId.isEmpty() && cmd.equals("01")) {
-                Map<String, String> data = new HashMap<>();
+                Map<String, Object> data = new HashMap<>();
                 String deviceType = api.getDeviceType();
                 switch (deviceType) {
                     case "Heart Rate Device":
                         data.put("deviceId", deviceId);
                         data.put("deviceType", "1");
-                        data.put("heartRate", api.getHeartrate());
+                        String heartRate = api.getHeartrate();
+                        if (isNotValid(heartRate)) {
+                            return;
+                        }
+                        data.put("deviceName", "HeartRate Monitor");
+                        data.put("heartRate", Integer.parseInt(heartRate));
                         data.put("calorie", api.getCalorie());
                         data.put("step", api.getStep());
                         data.put("rssi", api.getRssi());
@@ -87,30 +97,72 @@ public class CL900Runner implements ApplicationRunner {
                     case "Rope skipping":
                         data.put("deviceId", deviceId);
                         data.put("deviceType", "2");
-                        data.put("count", api.getCounter());
+                        String count = api.getCounter();
+                        if (isNotValid(count)) {
+                            return;
+                        }
+                        data.put("deviceName", "Rope Skipping");
+                        data.put("count", count);
                         data.put("time", api.getTime());
                         data.put("rssi", api.getRssi());
                         break;
                     case "cadence":
                         data.put("deviceId", deviceId);
                         data.put("deviceType", "3");
-                        data.put("cadence", api.getCadence());
+                        String cadence = api.getCadence();
+                        if (isNotValid(cadence)) {
+                            return;
+                        }
+                        data.put("deviceName", "Cadence");
+                        data.put("cadence", cadence);
                         data.put("rssi", api.getRssi());
                         break;
                     case "speed":
                         data.put("deviceId", deviceId);
                         data.put("deviceType", "4");
-                        data.put("speed", api.getSpeed());
+                        String speed = api.getSpeed();
+                        if (isNotValid(speed)) {
+                            return;
+                        }
+                        data.put("deviceName", "Cadence");
+                        data.put("speed", speed);
+                        //计算距离,记录首次开始时间
+                        long current = System.currentTimeMillis();
+                        if (mCadence.get(deviceId) == null) {
+                            mCadence.put(deviceId, current);
+                            data.put("distance", 0);
+                        } else {
+                            long start = mCadence.get(deviceId);
+                            long delta = current - start;
+                            if (delta > 0) {
+                                //(((delta / 1000) * 1000) / (60 * 60))
+                                int distance = (int) (delta / 3600);
+                                data.put("distance", distance);
+                            } else {
+                                data.put("distance", "");
+                            }
+                        }
                         data.put("rssi", api.getRssi());
                         break;
                 }
                 if (!data.isEmpty()) {
                     WebSocketServer.sendMessage(JSONObject.toJSONString(data), null);
+                    long current = System.currentTimeMillis();
+                    if (mCacheTime.get(deviceId) != null) {
+                        mCacheTime.replace(deviceId, current);
+                    } else {
+                        mCacheTime.put(deviceId, current);
+                    }
                 }
             }
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
+
+    private boolean isNotValid(String value) {
+        return value.isEmpty() || Float.parseFloat(value) == 0f;
+    }
+
 }
 
